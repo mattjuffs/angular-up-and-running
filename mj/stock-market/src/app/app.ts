c47474ca2de689ed7899d78f56af3e219ac28294
Matt Juffs <matthew.juffs@microlise.com>
2025-08-01 16:04:08 +0100

    Added app-stock-item
    The component app-stock-item has been added to the main app component, to be rendered by Angular.

--- mj/stock-market/src/app/app.ts
+++ mj/stock-market/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { StockItem } from './stock/stock-item/stock-item';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,StockItem],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- mj/stock-market/src/app/stock/stock-item/stock-item.ts
+++ mj/stock-market/src/app/stock/stock-item/stock-item.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-stock-item',
@@ -6,6 +6,38 @@ import { Component } from '@angular/core';
   templateUrl: './stock-item.html',
   styleUrl: './stock-item.css'
 })
-export class StockItem {
+export class StockItem implements OnInit {
+  public name: string = "";
+  public code: string = "";
+  public price: number = 0;
+  public previousPrice: number = 0;
+  public positiveChange: boolean = false;
 
+  constructor() {}
+
+  ngOnInit(): void {
+    this.name = "Slickhouse";
+    this.code = "SHP";
+    this.price = 85;
+    this.previousPrice = 80;
+    this.positiveChange = this.price >= this.previousPrice;
+
+    /*for (let index = 0; index < 10; index++) {
+      (async () => {
+        console.log("before delay");
+        await this.delay(1000);
+        this.updatePrice;
+        console.log("after delay");
+      })();
+    }*/
+  }
+
+  /*updatePrice(): void {
+    this.price++;
+  }*/
+
+  // https://community.lambdatest.com/t/how-to-implement-delay-in-typescript-for-redirect-after-form-submission/31676
+  /*delay(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }*/
 }
