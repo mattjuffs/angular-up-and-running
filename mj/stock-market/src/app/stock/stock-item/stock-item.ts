76fae3b9b84d660e6abd6589eeeb47e1cdd85479
Matt Juffs <matthew.juffs@microlise.com>
2025-08-01 14:59:32 +0100

    Added mj sub-folder for my code
    Using latest Angular 20, so code differs from the examples in the book (Angular 5).

---
+++ mj/stock-market/src/app/stock/stock-item/stock-item.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stock-item',
+  imports: [],
+  templateUrl: './stock-item.html',
+  styleUrl: './stock-item.css'
+})
+export class StockItem {
+
+}
