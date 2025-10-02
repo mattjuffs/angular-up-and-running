715421c12b442bbf2618b56f26982971514e5201
Matt Juffs <matt@slickhouse.com>
2025-10-02 16:35:02 +0100

    Chapter 3
    Completed Chapter 3 - using built-in Angular Directives

--- mj/stock-market173/src/app/stock/stock-item/stock-item.component.ts
+++ mj/stock-market173/src/app/stock/stock-item/stock-item.component.ts
@@ -21,6 +21,10 @@ export class StockItemComponent implements OnInit {// implement OnInit interface
   */
 
   public stock: Stock;
+  public stockClasses;// JSON object to hold the CSS classes
+  public stockStyles;// JSON object to hold the CSS styles
+
+  public stocks: Array<Stock>;
 
   constructor() { }
 
@@ -33,18 +37,50 @@ export class StockItemComponent implements OnInit {// implement OnInit interface
     this.positiveChange = (this.price >= this.previousPrice);
     this.favourite = false;*/
 
-    this.stock = new Stock("Test Stock Company", "TSC", 85, 80);
+    //this.stock = new Stock("Test Stock Company", "TSC", 85, 80);
+
+    //let diff = (this.stock.price / this.stock.previousPrice) - 1;
+    //let largeChange = Math.abs(diff) > 0.01;
+
+    /*this.stockClasses = {
+        "positive": this.stock.isPositiveChange(),
+        "negative": !this.stock.isPositiveChange(),
+        "large-change": largeChange,
+        "small-change": !largeChange
+    };*/
+
+    /*this.stockStyles = {
+      "color": this.stock.isPositiveChange() ? "green" : "red",
+      "font-size": largeChange ? "1.2em" : "0.8em"
+    }*/
+
+    // array of stocks
+    this.stocks = [
+      new Stock('1st Company', 'TSC', 85, 80, "stock"),
+      new Stock('2nd Company', 'SSC', 10, 20, "mjtest"),
+      new Stock('3rd Company', '3SC', 80, 80, "stock"),
+      new Stock('4th Company', '4SC', 876, 765, "")
+    ];
   }
 
   // event function - trigger on click
-  toggleFavourite(event) {
-    console.log("toggleFavourite() called!", event);
+  toggleFavourite(event, index) {
+    console.log("toggleFavourite() called!", index, event);
     //this.favourite = !this.favourite;
 
     // update the price
     //this.stock.previousPrice = this.stock.price;
     //this.stock.price = 75;
 
-    this.stock.favourite = !this.stock.favourite;
+    //this.stock.favourite = !this.stock.favourite;
+
+    // using index (of array)
+    this.stocks[index].favourite = !this.stocks[index].favourite;
+  }
+
+  // use this to track an object in an array by stock.code, rather than using the DOM object reference
+  // helpful when reloading/amending the DOM and array, to keep track of items
+  trackByStockCode(index, stock) {
+    return stock.code;
   }
 }
